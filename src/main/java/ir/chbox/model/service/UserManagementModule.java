7930303334a082a75b20a894ebf0e12d629d6b9e
farzad <farzad.sedaghatbin@gmail.com>
2015-11-25 17:38:07 +0330

    first Push Code

--- src/main/java/ir/chbox/model/service/UserManagementModule.java
+++ src/main/java/ir/chbox/model/service/UserManagementModule.java
@@ -3,21 +3,36 @@ package ir.chbox.model.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ir.chbox.model.dao.UserDAOImpl;
 import ir.chbox.model.entity.core.User;
+import ir.chbox.utils.ImageUtils;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.FileOutputStream;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.OutputStream;
 
 @Path("/service")
 public class UserManagementModule {
     @POST
     @Path("/receiveMedia")
     public Response getAllUsers(String s) {
+        boolean picture=true;
         byte[] data = Base64.decodeBase64(s.substring(24,s.length()-1));
+        if(picture) {
+            BufferedImage b = ImageUtils.convertByteArrayToBufferedImage(data);
+            float scaleRatio = ImageUtils.calculateScaleRatio(b.getWidth(), 768);//todo from properties
+            if (scaleRatio > 0 && scaleRatio < 1) {
+                try {
+                    BufferedImage sourceBufferedImage = ImageUtils.scaleImage(b, scaleRatio);
+                    data=ImageUtils.imageToByteArray(sourceBufferedImage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else{
+
+        }
 
         new S3Service().doSave(data);
         String result = "<h1>A test File saved in the bucket</h1>";
--- src/main/java/ir/chbox/utils/video/MediaConvertor.java
+++ src/main/java/ir/chbox/utils/video/MediaConvertor.java
@@ -3,33 +3,73 @@ package ir.chbox.utils.video;
 import com.xuggle.mediatool.IMediaReader;
 import com.xuggle.mediatool.IMediaWriter;
 import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IContainerFormat;
+
+import java.io.*;
+import java.util.Date;
 
 public class MediaConvertor {
-	private static final Integer WIDTH = 320;
-	private static final Integer HEIGHT = 240;
- 
-	private static final String INPUT_FILE = "/home/farzad/dr/s4.mp4";
-	private static final String OUTPUT_FILE = "/home/farzad/dr/out.mp4";
- 
-	public static void main(String[] args) {
-		// create custom listeners
-		MyVideoListener myVideoListener = new MyVideoListener(WIDTH, HEIGHT);
-		Resizer resizer = new Resizer(WIDTH, HEIGHT);
+    private static final Integer WIDTH = 320;
+    private static final Integer HEIGHT = 240;
 
-		// reader
-		IMediaReader reader = ToolFactory.makeReader(INPUT_FILE);
-		reader.addListener(resizer);
- 
-		// writer
-		IMediaWriter writer = ToolFactory.makeWriter(OUTPUT_FILE, reader);
-		resizer.addListener(writer);
-		writer.addListener(myVideoListener);
- 
-		// show video when encoding
-		reader.addListener(ToolFactory.makeViewer(true));
- 
-		while (reader.readPacket() == null) { 
-			// continue coding
-		}
-	}
+    private static final String INPUT_FILE = "/home/farzad/dr/s4.mp4";
+    private static final String OUTPUT_FILE = "/home/farzad/dr/out.mp4";
+    private static IContainer mIContainer = null;
+    /**
+     * A container we'll use to write data from.
+     */
+    private static IContainer mOContainer = null;
+
+    public static void main(String[] args) {
+        try {
+
+
+        // create custom listeners
+        MyVideoListener myVideoListener = new MyVideoListener(WIDTH, HEIGHT);
+        Resizer resizer = new Resizer(WIDTH, HEIGHT);
+        mIContainer = IContainer.make();
+        mOContainer = IContainer.make();
+        IContainerFormat iFmt = null;
+        IContainerFormat oFmt = null;
+
+
+        // override input format
+        iFmt = IContainerFormat.make();
+
+        /**
+         * Try to find an output format based on what the user specified, or
+         * failing that, based on the outputURL (e.g. if it ends in .flv, we'll
+         * guess FLV).
+         */
+        byte[] data = null;
+        mIContainer.open(new DataInputStream(new ByteArrayInputStream(data)), iFmt);
+        oFmt = IContainerFormat.make();
+        File file = File.createTempFile(new Date().toString(), ".mp4");
+        file.deleteOnExit();
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        mOContainer.open(new DataOutputStream(fileOutputStream), oFmt);
+
+
+
+        // reader
+        IMediaReader reader = ToolFactory.makeReader(mIContainer);
+        reader.addListener(resizer);
+
+        // writer
+        IMediaWriter writer = ToolFactory.makeWriter(file.getAbsolutePath(), reader);
+        resizer.addListener(writer);
+        writer.addListener(myVideoListener);
+
+        // show video when encoding
+//        reader.addListener(ToolFactory.makeViewer(true));
+
+        while (reader.readPacket() == null) {
+            // continue coding
+        }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
